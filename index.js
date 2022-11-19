ace0c847505aab4b4bec7a5370f7fc40c2bc6926
Emmanuel <emmanuel.vouillon@epita.fr>
2022-11-19 15:12:51 +0100

    fixes item count

--- index.js
+++ index.js
@@ -42,6 +42,10 @@
                             $('#list-container').append("<div class='list-item' onclick='addItem(" + i + ")'><div id='list-icon'><img src='list-icon.png' height='44px'></div><div><p id='item-name'>" + vocab[i].name + "</p><p id='item-counter'>" + vocab[i].content.length + " items</p></div><div class='checker'><img id='status-image" + i + "' class='list-status' src=''></div>");
             }
             document.getElementById('list-count').innerHTML = "<img src='list-icon-gray.png'><p>studying " + selected.length + " lists</p>";
+            s = 0;
+            for (i = 0; i < selected.length; i++)
+                s += vocab[selected[i]].content.length;
+            document.getElementById('item-count').innerHTML = "<img src='list-icon-gray.png'><p>studying " + s + " items</p>";
         }
 
         function addItem(n) {
@@ -55,6 +59,10 @@
                 document.getElementById("status-image" + n).src = "check-icon.png";
             }
             document.getElementById('list-count').innerHTML = "<img src='list-icon-gray.png'><p>studying " + selected.length + " lists</p>";
+            s = 0;
+            for (i = 0; i < selected.length; i++)
+                s += vocab[selected[i]].content.length;
+            document.getElementById('item-count').innerHTML = "<img src='list-icon-gray.png'><p>studying " + s + " items</p>";
         }
 
         function start() {
