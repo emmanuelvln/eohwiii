9e3e83a9cbb9f0bc33d229d3a4e67f79cefdcd54
Emmanuel <emmanuel.vouillon@epita.fr>
2023-03-06 18:02:24 +0900

    adds a droop down menu to list items

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 vocab = null;
 selected = [];
+opened = null;
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -31,15 +32,37 @@ function setupData() {
         if (selected == null)
             selected = [];
         if (selected != [] && selected.includes(i))
-            $('#list-container').append("<div class='list-item'><div id='chevron'><img id='chevron-icon' src='imgs/chevron.webp' height='20px' weight='20xp'></div><div id='list-icon' onclick='addItem(" + i + ")'><img src='imgs/list-icon.png' height='40px'></div><div onclick='addItem(" + i + ")'><p id='item-name'>" + vocab[i].name + "</p><p id='item-counter'>" + vocab[i].content.length + " items</p></div><div class='checker' onclick='addItem(" + i + ")'><img id='status-image" + i + "' class='list-status' src='imgs/check-icon.png'></div>");
+            $('#list-container').append("<div class='list-item-container'><div id='list-item" + i + "' class='list-item'><div class='chevron' onclick='openItem(" + i + ")'><img id='chevron" + i + "' class='chevron-icon' src='imgs/chevron-right.png' height='20px' weight='20xp'></div><div id='list-icon' onclick='addItem(" + i + ")'><img src='imgs/list-icon.png' height='40px'></div><div onclick='addItem(" + i + ")'><p id='item-name'>" + vocab[i].name + "</p><p id='item-counter'>" + vocab[i].content.length + " items</p></div><div class='checker' onclick='addItem(" + i + ")'><img id='status-image" + i + "' class='list-status' src='imgs/check-icon.png'></div></div><div id='hidden-list-item" + i + "' class='hidden-list-item'></div></div>");
         else
-            $('#list-container').append("<div class='list-item'><div id='chevron'><img id='chevron-icon' src='imgs/chevron.webp' height='20px' weight='20px'></div><div id='list-icon' onclick='addItem(" + i + ")'><img src='imgs/list-icon.png' height='40px'></div><div onclick='addItem(" + i + ")'><p id='item-name'>" + vocab[i].name + "</p><p id='item-counter'>" + vocab[i].content.length + " items</p></div><div class='checker' onclick='addItem(" + i + ")'><img id='status-image" + i + "' class='list-status' src=''></div>");
+            $('#list-container').append("<div class='list-item-container'><div id='list_item" + i  + "' class='list-item'><div class='chevron' onclick='openItem(" + i + ")'><img id='chevron" + i + "' class='chevron-icon' src='imgs/chevron-right.png' height='20px' weight='20px'></div><div id='list-icon' onclick='addItem(" + i + ")'><img src='imgs/list-icon.png' height='40px'></div><div onclick='addItem(" + i + ")'><p id='item-name'>" + vocab[i].name + "</p><p id='item-counter'>" + vocab[i].content.length + " items</p></div><div class='checker' onclick='addItem(" + i + ")'><img id='status-image" + i + "' class='list-status' src=''></div><div id='hidden-list-item" + i + "' class='hidden-list-item'></div></div>");
     }
     document.getElementById('list-count').innerHTML = "<img src='imgs/list-icon-gray.png'><p>studying " + selected.length + " lists</p>";
     s = 0;
     for (i = 0; i < selected.length; i++)
         s += vocab[selected[i]].content.length;
     document.getElementById('item-count').innerHTML = "<img src='imgs/item-icon.png'><p>studying " + s + " items</p>";
+}
+
+function openItem(n) {
+    if (n == opened)
+    {
+        document.getElementById("hidden-list-item" + n).style.display = "none";
+        document.getElementById("chevron" + n).src = "imgs/chevron-right.png";
+        opened = null;
+    }
+    else if (opened != null)
+    {
+        document.getElementById("hidden-list-item" + opened).style.display = "none";
+        document.getElementById("hidden-list-item" + n).style.display = "block";
+        document.getElementById("chevron" + n).src = "imgs/chevron-down.png";
+        opened = n;
+    }
+    else
+    {
+        document.getElementById("hidden-list-item" + n).style.display = "block";
+        document.getElementById("chevron" + n).src = "imgs/chevron-down.png";
+        opened = n;
+    }
 }
 
 function addItem(n) {
